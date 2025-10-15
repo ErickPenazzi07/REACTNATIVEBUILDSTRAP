61ad9b3d65cb465d3877d958ea9cf9543de14c28
DevErick <djerickdev@gmail.com>
2025-10-15 10:45:56 -0300

    Enhance HomeScreen and SensorDHTScreen with theme toggle and carousel; add custom styles

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import { View, Text } from 'react-native';
-import { createStyles } from 'react-native-buildstrap';
+import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { createStyles, useTheme } from "react-native-buildstrap";
+import { customStyles } from "../styles/CustomStyles";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen() {
-    const styles =  createStyles();
+  const styles = createStyles(customStyles());
+  const { toggleTheme, isDark } = useTheme();
 
   return (
-    <View style={styles.container}>
-<Text style={styles.text}>Pagina Inicial</Text>
-
-
+    <View style={[styles.container, styles.bgToggle]}>
+      <Text style={[styles.textToggle, styles.mb1]}>Página Inicial</Text>
+      <TouchableOpacity
+        onPress={toggleTheme}
+        style={[styles.bgInfo, styles.px2, styles.py1, styles.roundedCircle]}
+      >
+        {isDark ? <Ionicons name="sunny" size={24} color="white"></Ionicons> : <Ionicons name="moon" size={24} color="white"></Ionicons>}
+      </TouchableOpacity>
+      <Text style={[styles.mt2, styles.text]}>Meu estilo próprio</Text>
+      <StatusBar barStyle={isDark ? "light-content" : "dark-content"}/>
     </View>
   );
 }
--- Screens/SensorDHTScreen.js
+++ Screens/SensorDHTScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
-import { createStyles } from 'react-native-buildstrap';
+import { Text, View } from "react-native";
+import { createStyles } from "react-native-buildstrap";
+import { Carousel } from "react-native-buildstrap/src/components/Carousel";
 
 export default function SensorDHTScreen() {
+  const styles = createStyles();
 
+  const data = [
+    {
+      image: "https://picsum.photos/800/400/1",
+      title: "Sensores",
+      description: "descritivo",
+    },
+    {
+      image: "https://picsum.photos/800/400/1",
+      title: "Sensores",
+      description: "descritivo",
+    },
+    {
+      image: "https://picsum.photos/800/400/1",
+      title: "Sensores",
+      description: "descritivo",
+    },
+  ];
 
   return (
-
+    <View style={[styles.container, styles.bgToggle]}>
+      <Carousel data={data} autoPlay interval={30000}/>
+    </View>
   );
 }
